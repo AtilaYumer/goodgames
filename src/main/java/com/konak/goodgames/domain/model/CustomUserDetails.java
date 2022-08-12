983abb3b930e90351567052df5ff85e7e37980a7
Atila Yumer <atillayumer@gmail.com>
2022-08-13 00:40:21 +0300

    Fix roles

--- src/main/java/com/konak/goodgames/domain/model/CustomUserDetails.java
+++ src/main/java/com/konak/goodgames/domain/model/CustomUserDetails.java
@@ -1,15 +1,21 @@
 package com.konak.goodgames.domain.model;
 
+import com.konak.goodgames.domain.enums.Role;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public record CustomUserDetails(User user) implements UserDetails {
 
   @Override
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    return null;
+    Role role = user.getRole().getRole();
+    Collection<GrantedAuthority> authorities = new ArrayList<>();
+    authorities.add(new SimpleGrantedAuthority(role.name()));
+    return authorities;
   }
 
   @Override
